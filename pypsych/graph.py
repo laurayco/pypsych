2b1269fa9e266985a46b7c16f9017e5b6f4399fa
Tyler Elric <tyler9725@gmail.com>
2015-09-02 11:53:12 -0500

    Initial Commit

---
+++ pypsych/graph.py
@@ -0,0 +1,113 @@
+﻿from hashlib import md5 as hash
+from time import time
+
+class Node:
+    def __init__(self):
+        pass
+
+class Edge:
+    def __init__(self, a, b):
+        self.left = a
+        self.right = b
+
+class Group:
+    def __init__(self, members):
+        self.members = members
+
+class ViewResults:
+    def __init__(self, data):
+        self.data = data
+        self.filtered_data = data
+        self.position = 0
+        self.length_lock = None #prevent length checking while filtering.
+    def __iter__(self):
+        return self.filtered_data.__iter__()
+    def filter(self, filt):
+        self.filtered_data = filter(filt,self.data)
+    def sort(self, func, reverse=False):
+        self.data.sort(key=func,reverse=reverse)
+
+class View:
+    def __init__(self):
+        pass
+    def map(self, document):
+        pass
+    def reduce(self, results):
+        return results
+
+class Database:
+    def __init__(self, storage):
+        self.storage = storage
+        self.views = {}
+    def register_view(self, name, view):
+        self.storage.introduce_view(name)
+        self.views[name.lower()] = view
+    def query_view(self, name):
+        return ViewResults(self.storage.view_results(name))
+    def write(self, doc, id=None):
+        if id:
+            self.storage.update_document(id,doc)
+        else:
+            id = self.storage.create_document(doc)
+        doc = self.storage.get_document(id)
+        for name, v in self.views.items():
+            result = v.map(doc)
+            if result:
+                self.storage.add_map_result(name, id, result)
+                total_results = self.storage.map_results(name)
+                self.storage.store_reduced_results(name, v.reduce(list(total_results.values())))
+
+class Storage:
+    def __init__(self):
+        self.documents = {}
+        self.view_storage = {}
+    def get_document(self,id):
+        return self.documents[id]
+    def update_document(self, id, doc):
+        self.documents[id] = {
+            '__id__':self.documents[id]['__id__'],
+            'modified':time(),
+            'created':self.documents[id]['created'],
+            'doc':doc
+        }
+    def introduce_view(self,name):
+        self.view_storage[name] = {
+            "map":{},
+            "reduced":[]
+        }
+    def create_document(self, doc):
+        id = self.create_id(doc)
+        t = time()
+        self.documents[id] = {
+            '__id__':id,
+            'modified':t,
+            'created':t,
+            'doc':doc
+        }
+        return id
+    def create_id(self, doc):
+        h = hash()
+        h.update(str(doc).encode('utf-8'))
+        return h.hexdigest()
+    def add_map_result(self, viewname, docid, result):
+        if viewname not in self.view_storage:
+            self.view_storage[viewname] = {
+                'map':{
+                    docid: result
+                },
+                'reduced':[]
+            }
+        else:
+            self.view_storage[viewname]['map'][docid] = result
+    def store_reduced_results(self, viewname, results):
+        if viewname not in self.view_storage:
+            self.view_storage[viewname] = {
+                'map':{},
+                'reduced': results
+            }
+        else:
+            self.view_storage[viewname]['reduced'] = results
+    def map_results(self, viewname):
+        return self.view_storage[viewname]['map']
+    def view_results(self, viewname):
+        return self.view_storage[viewname]['reduced']
---
+++ pypsych/pypsych.py
@@ -0,0 +1,106 @@
+﻿import bottle
+from itertools import combinations, starmap
+from graph import Storage, View, Database
+
+class UserView(View):
+    def map(self,doc):
+        if doc['doc'].get("kind")=='user':# and doc['doc'].get("verified"):
+            return doc
+    def reduce(self,docs):
+        return docs
+
+class MatchesView(UserView):
+    def reduce(self,docs):
+        f = lambda pair:pair[2]["overall"] >= max(pair[0]['doc']["match_requirement"],pair[1]['doc']["match_requirement"])
+        return list(starmap(self.make_match,filter(f,starmap(self.score,combinations(docs,2)))))
+    def score(self,a,b):
+        score = {
+            "aspects":{
+                "test_property":1
+            }
+        }
+        score["overall"] = sum(score["aspects"].values())
+        return (a,b,score)
+    def make_match(self,a,b,score):
+        return [a['__id__'],b['__id__'],score]
+
+class MessageView(View):
+    def map(self,doc):
+        return doc if doc.get("kind")=='msg' else None
+    def reduce(self,docs):
+        return docs
+
+class WebApp:
+    def __init__(self,db):
+        self.db = db
+        self.db.register_view("users",UserView())
+        self.db.register_view("matches",MatchesView())
+        self.db.register_view("messages",MessageView())
+    def user_exists(self,uid):
+        for user in self.db.query_view("users"):
+            if user['__id__']==uid:
+                return True
+        return False
+    def create_user(self,userinfo):
+        userinfo["email"] = userinfo["email"].lower()
+        for user in self.db.query_view("users"):
+            assert user["doc"]["email"]!=userinfo["email"]
+        userinfo["verified"]=False
+        userinfo['kind'] = 'user'
+        userinfo["match_requirement"] = 1
+        return self.db.write(userinfo)
+    def send_message(self,sender,receiver,content):
+        if self.user_exists(sender) and self.user_exists(receiver):
+            self.db.write({
+                "content":content,
+                "sender":sender,
+                "receiver":receiver
+            })
+    def run_match_search(self,uid,filts=None,sorts=None):
+        base = self.db.query_view("matches")
+        base.filter(lambda match:match[0]==uid or match[1]==uid)
+        for f in (filts or []):
+            base.filter(f)
+        for s in (sorts or []):
+            base.sort(s)
+        yield from base
+    def is_ajax(self,req):
+        return True
+    def respond_to_index(self,req):
+        return "INDEX"
+    def get_user_data(self,uid):
+        #uid = req.query.get("uid")
+        assert self.user_exists(uid)
+        base = self.db.query_view("users")
+        base.filter(lambda u:u['__id__']==uid)
+        base = list(base)
+        assert len(base) > 0
+        return base[0]
+    def respond_to_user(self,req):
+        if self.is_ajax(req):
+            if req.method=="GET":#get existing data.
+                return self.get_user_data(req.query["uid"])
+            if req.method=="POST":#create new user.
+                return self.create_user(req.query)
+            if req.method=="DELETE":#delete a user.
+                if False:
+                    self.db.destroy(req.query["uid"])
+            if req.method=="PUT":
+                self.db.write(req.query,req.query["uid"])
+
+if __name__ == "__main__":
+    app = WebApp(Database(Storage()))
+    tid = app.create_user({
+        "email":"tyler9725@gmail.com",
+        "username":"tyler.elric",
+        "hobbies":["lol"],
+    })
+    app.create_user({
+        "email":"xlahr@facebook.com",
+        "username":"firagastorm",
+        "hobbies":["lol"]
+    })
+    for match in app.db.query_view("matches"):
+        a = app.get_user_data(match[0])
+        b = app.get_user_data(match[1])
+        print(a["doc"]["username"],"and",b["doc"]["username"])
