136275c438b9e70c4cbbd8c15a6da2cf855c8c89
Tyler Elric <tyler9725@gmail.com>
2015-09-02 20:50:04 -0500

    Renamed WebApp to ApiControl, removed appropriate methods.

--- pypsych/pypsych.py
+++ pypsych/pypsych.py
@@ -7,16 +7,20 @@ from itertools import combinations, starmap
 from graph import Storage, View, Database
 
 class UserView(View):
+    
     def map(self,doc):
         if doc['doc'].get("kind")=='user':# and doc['doc'].get("verified"):
             return doc
+    
     def reduce(self,docs):
         return docs
 
 class MatchesView(UserView):
+
     def reduce(self,docs):
         f = lambda pair:pair[2]["overall"] >= max(pair[0]['doc']["match_requirement"],pair[1]['doc']["match_requirement"])
         return list(starmap(self.make_match,filter(f,starmap(self.score,combinations(docs,2)))))
+
     def score(self,a,b):
         score = {
             "aspects":{
@@ -25,6 +29,7 @@ class MatchesView(UserView):
         }
         score["overall"] = sum(score["aspects"].values())
         return (a,b,score)
+
     def make_match(self,a,b,score):
         return [a['__id__'],b['__id__'],score]
 
@@ -45,7 +50,7 @@ class MessageView(View):
             conversation.sort(key=lambda msg:msg["timestamp"])
         return list(starmap((lambda conversations,messages:{"participants":conversations,"messages":messages}),conversations.items()))
 
-class WebApp:
+class ApiControl:
     
     def __init__(self,db):
         self.db = db
@@ -87,35 +92,16 @@ class WebApp:
             base.sort(s)
         yield from base
    
-    def is_ajax(self,req):
-        return True
-    
-    def respond_to_index(self,req):
-        return "INDEX"
-   
     def get_user_data(self,uid):
-        #uid = req.query.get("uid")
         assert self.user_exists(uid)
         base = self.db.query_view("users")
         base.filter(lambda u:u['__id__']==uid)
         base = list(base)
         assert len(base) > 0
         return base[0]
- 
-    def respond_to_user(self,req):
-        if self.is_ajax(req):
-            if req.method=="GET":#get existing data.
-                return self.get_user_data(req.query["uid"])
-            if req.method=="POST":#create new user.
-                return self.create_user(req.query)
-            if req.method=="DELETE":#delete a user.
-                if False:
-                    self.db.destroy(req.query["uid"])
-            if req.method=="PUT":
-                self.db.write(req.query,req.query["uid"])
 
 if __name__ == "__main__":
-    app = WebApp(Database(Storage()))
+    app = ApiControl(Database(Storage()))
     tid = app.create_user({
         "email":"tyler9725@gmail.com",
         "username":"tyler.elric",
